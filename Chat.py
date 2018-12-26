222bfd3e3c252948e706c332c36a464eb152dc22
Xi Chen <jasonxi89@gmail.com>
2018-12-25 21:41:09 -0800

    12/25/2018 Daily Update
    Pi 3B +  √
    NAS database √
    Begin to code for the core.
    Make a small program to collect all teh avatar from friends, and make it to a new picture.√
    Installed pymysql for python 3 √

---
+++ Chat.py
@@ -0,0 +1,14 @@
+import itchat
+import pymysql
+
+#Connect to local mysql
+conn = pymysql.Connect(host='192.168.31.1680', port=3306, user='test', password='123456', db='test', charset='utf8')
+
+
+
+
+
+
+
+
+
