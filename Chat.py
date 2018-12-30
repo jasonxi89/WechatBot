b6693e8f69ea900b5afaeb9790a651bcd012d68e
Xi Chen <jasonxi89@gmail.com>
2018-12-30 00:03:01 -0800

    12/29 update
    Finished SQL connection

--- Chat.py
+++ Chat.py
@@ -1,14 +1,28 @@
 import itchat
 import pymysql
 
-#Connect to local mysql
-conn = pymysql.Connect(host='192.168.31.1680', port=3306, user='test', password='123456', db='test', charset='utf8')
+#check database
+
+
+def sqlQuery(query):
+    conn = pymysql.connect(host='192.168.31.168', port=3307, user='test', password='123456', db='test',
+                           use_unicode=True)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    values = cursor.fetchall()
+    conn.close()
+    return values
+
+
+for row in sqlQuery('select * from orderStatus'):
+    print('利达单号'+ row[1] + ' ,国内快递号'+row[2])
 
 
 
 
-
-
-
+# if __name__ == "__main__":
+#     itchat.auto_login(enableCmdQR=2)
+#     itchat.run()
+#
 
 
